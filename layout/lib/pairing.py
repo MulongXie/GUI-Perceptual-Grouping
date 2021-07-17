0dbac30b4275b3371b502ff4e9dcf90a05e143ac
MulongXie <dsh15325@163.com>
2021-07-17 18:17:30 +1000

    match two groups by distance; fix some bugs in pair_matching

--- layout/lib/pairing.py
+++ layout/lib/pairing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import cv2
 import numpy as np
+import math
 
 import layout.lib.draw as draw
 
@@ -30,6 +31,55 @@ def match_two_groups(g1, g2, max_pos_bias):
             g2.loc[pairs[i], 'pair_to'] = i
         return True
     return False
+
+
+def match_two_groups_distance(g1, g2):
+    assert g1.iloc[0]['alignment_in_group'] == g2.iloc[0]['alignment_in_group']
+    alignment = g1.iloc[0]['alignment_in_group']
+    pairs = {}
+    if alignment == 'h':
+        min_side = min(list(g1['height']) + list(g2['height']))
+    else:
+        min_side = min(list(g1['width']) + list(g2['width']))
+
+    if len(g1) == len(g2):
+        distances = []
+        for i in range(len(g1)):
+            c1 = g1.iloc[i]
+            c2 = g2.iloc[i]
+            distance = math.sqrt((c1['center_column'] - c2['center_column'])**2 + (c1['center_row'] - c2['center_row'])**2)
+            # mismatch if too far
+            if distance > min_side * 2:
+                return False
+            # mismatch if it's too different from others
+            if i > 0:
+                if max(distance, distances[i-1]) > 2 * min(distance, distances[i-1]):
+                    return False
+            pairs[c1['id']] = c2['id']
+            distances.append(distance)
+    else:
+        distances = []
+        for i in range(len(g1)):
+            c1 = g1.iloc[i]
+            distance = None
+            for j in range(len(g2)):
+                c2 = g2.iloc[j]
+                d_cur = math.sqrt((c1['center_column'] - c2['center_column']) ** 2 + (c1['center_row'] - c2['center_row']) ** 2)
+                if distance is None or distance < d_cur:
+                    distance = d_cur
+                    pairs[c1['id']] = c2['id']
+            # mismatch if too far
+            if distance > min_side * 2:
+                return False
+            # mismatch if it's too different from others
+            if i > 0:
+                if max(distance, distances[i-1]) > 2 * min(distance, distances[i-1]):
+                    return False
+            distances.append(distance)
+    for i in pairs:
+        g1.loc[i, 'pair_to'] = pairs[i]
+        g2.loc[pairs[i], 'pair_to'] = i
+    return True
 
 
 def pair_matching_between_multi_groups(groups1, groups2):
@@ -67,20 +117,29 @@ def pair_matching_within_groups(groups, new_pairs=True):
         for j in range(i + 1, len(groups)):
             g2 = groups[j]
             alignment2 = g2.iloc[0]['alignment_in_group']
-            if alignment1 == alignment2 and abs(len(g1) - len(g2)) <= 2:
-                if match_two_groups(g1, g2, 10):
+            if alignment1 == alignment2:
+                if match_two_groups_distance(g1, g2):
+                    # print(i, list(g1['group'])[0], mark[i], '-', j, list(g2['group'])[0], mark[j])
                     if not mark[i]:
                         # hasn't paired yet, creat a new pair
                         pair_id += 1
                         g1['group_pair'] = pair_id
+                        g2['group_pair'] = pair_id
                         pairs[pair_id] = [g1, g2]
                         mark[i] = True
                         mark[j] = True
                     else:
                         if not mark[j]:
+                            g2['group_pair'] = g1.iloc[0]['group_pair']
                             pairs[g1.iloc[0]['group_pair']].append(g2)
                             mark[j] = True
-                    g2['group_pair'] = pair_id
+                        # if gi and gj are all already marked in different group_pair, merge the two group_pairs together
+                        else:
+                            # merge all g2's pairing groups with g1's
+                            for g in pairs[g2.iloc[0]['group_pair']]:
+                                g['group_pair'] = g1.iloc[0]['group_pair']
+                                pairs[g1.iloc[0]['group_pair']].append(g)
+                            pairs.pop(g2.iloc[0]['group_pair'])
 
     merged_pairs = None
     for i in pairs:
@@ -103,7 +162,7 @@ def pair_visualization(pairs, img, img_shape, show_method='line'):
         for id in pairs:
             pair = pairs[id]
             for p in pair:
-                board = draw.visualize_block(board, p.compos_dataframe, img_shape, attr='group_pair', show=False)
+                board = draw.visualize_fill(board, p.compos_dataframe, img_shape, attr='group_pair', show=False)
     cv2.imshow('pairs', board)
     cv2.waitKey()
     cv2.destroyAllWindows()
