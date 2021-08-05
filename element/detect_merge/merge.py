4d7b0e20a6c0dfcf6533da96a64ed2f1c029fe4c
MulongXie <dsh15325@163.com>
2021-08-05 12:52:32 +1000

    add id for each element; save the children id instead of entire elemenets

--- element/detect_merge/merge.py
+++ element/detect_merge/merge.py
@@ -10,7 +10,7 @@ from element.detect_merge.Element import Element
 
 
 def show_elements(org_img, eles, show=False, win_name='element', wait_key=0, shown_resize=None, line=2):
-    color_map = {'Text':(0, 0, 255), 'Compo':(0, 255, 0), 'Text Content':(255, 0, 255)}
+    color_map = {'Text':(0, 0, 255), 'Compo':(0, 255, 0), 'Block':(0, 255, 255), 'Text Content':(255, 0, 255)}
     img = org_img.copy()
     for ele in eles:
         color = color_map[ele.category]
@@ -30,10 +30,15 @@ def save_elements(output_file, elements, img_shape):
     components = {'compos': [], 'img_shape': img_shape}
     for i, ele in enumerate(elements):
         c = ele.wrap_info()
-        c['id'] = i
+        # c['id'] = i
         components['compos'].append(c)
     json.dump(components, open(output_file, 'w'), indent=4)
     return components
+
+
+def reassign_ids(elements):
+    for i, element in enumerate(elements):
+        element.id = i
 
 
 def refine_texts(texts, img_shape):
@@ -67,15 +72,28 @@ def refine_elements(compos, texts, intersection_bias=2, containment_ratio=0.8):
                 if iob >= containment_ratio:
                     contained_texts.append(text)
         if is_valid and text_area / compo.area < containment_ratio:
-            for t in contained_texts:
-                t.is_child = True
-            compo.children += contained_texts
+            # for t in contained_texts:
+            #     t.parent_id = compo.id
+            # compo.children += contained_texts
             elements.append(compo)
 
-    for text in texts:
-        if not text.is_child:
-            elements.append(text)
+    elements += texts
+    # for text in texts:
+    #     if not text.is_child:
+    #         elements.append(text)
     return elements
+
+
+def check_containment(elements):
+    for i in range(len(elements) - 1):
+        for j in range(i + 1, len(elements)):
+            relation = elements[i].element_relation(elements[j], bias=2)
+            if relation == -1:
+                elements[j].children.append(elements[i])
+                elements[i].parent_id = elements[j].id
+            if relation == 1:
+                elements[i].children.append(elements[j])
+                elements[j].parent_id = elements[i].id
 
 
 def remove_top_bar(elements, img_height):
@@ -136,14 +154,17 @@ def merge(img_path, compo_path, text_path, merge_root=None, is_remove_top=True, 
     text_json = json.load(open(text_path, 'r'))
 
     # load text and non-text compo
+    ele_id = 0
     compos = []
     for compo in compo_json['compos']:
-        element = Element((compo['column_min'], compo['row_min'], compo['column_max'], compo['row_max']), compo['class'])
+        element = Element(ele_id, (compo['column_min'], compo['row_min'], compo['column_max'], compo['row_max']), compo['class'])
         compos.append(element)
+        ele_id += 1
     texts = []
     for text in text_json['texts']:
-        element = Element((text['column_min'], text['row_min'], text['column_max'], text['row_max']), 'Text', text_content=text['content'])
+        element = Element(ele_id, (text['column_min'], text['row_min'], text['column_max'], text['row_max']), 'Text', text_content=text['content'])
         texts.append(element)
+        ele_id += 1
     if compo_json['img_shape'] != text_json['img_shape']:
         resize_ratio = compo_json['img_shape'][0] / text_json['img_shape'][0]
         for text in texts:
@@ -158,11 +179,13 @@ def merge(img_path, compo_path, text_path, merge_root=None, is_remove_top=True, 
     texts = refine_texts(texts, compo_json['img_shape'])
     elements = refine_elements(compos, texts)
     if is_remove_top: elements = remove_top_bar(elements, img_height=compo_json['img_shape'][0])
+    reassign_ids(elements)
+    check_containment(elements)
     board = show_elements(img_resize, elements, show=show, win_name='elements after merging', wait_key=wait_key)
 
     # save all merged elements, clips and blank background
-    name = img_path.split('/')[-1][:-4]
+    name = img_path.split('/')[-1][:-4] if '/' in img_path else img_path.split('\\')[-1][:-4]
     components = save_elements(pjoin(merge_root, name + '.json'), elements, img_resize.shape)
     cv2.imwrite(pjoin(merge_root, name + '.jpg'), board)
-    print('Merge Complete and Save to', pjoin(merge_root, name + '.jpg'), time.ctime(), '\n')
+    print('[Merge Completed] Input: %s Output: %s' % (img_path, pjoin(merge_root, name + '.jpg')))
     return board, components
