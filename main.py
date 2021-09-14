8ab7168d78fde14625db3d6e051b8ff1e1ac8bff
MulongXie <dsh15325@163.com>
2021-09-14 16:27:32 +1000

    add main function; merge texts contained in non-block compos

---
+++ main.py
@@ -0,0 +1,11 @@
+from GUI import GUI
+
+
+input_path = 'data/input/1565.jpg'
+output_root = 'data/output'
+
+gui = GUI(img_file=input_path, output_dir=output_root)
+gui.element_detection(True, True, True)
+gui.visualize_element_detection()
+gui.layout_recognition()
+gui.visualize_layout_recognition()
